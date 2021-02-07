4f2479273d8dc1e55c21941af9f1f8fdab7350a6
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-07 18:11:34 +0300

    feat: code update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort(matrix) {
-  if (!matrix) return [];
-  return matrix.reduce((acc, item, index) => {
-    if (index % 2 == 0) {
-      acc.push(...item);
-    } else {
-      acc.push(...item.reverse());
-    }
-    return acc;
-  }, []);
-}
+module.exports = towelSort(matrix) {
+    let towelArr = [];
+    return towelArr.concat(...matrix);
+  };
